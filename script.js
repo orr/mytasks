0ed266d39e1cb35d48c6e948372d21dfe0116a9e
otavio <otavio.ramos@gmail.com>
2022-09-28 21:39:55 -0300

    js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function createTask(task) {
 
     let template = '<div class="category #bgcolor#"><span class="material-icons">#icon#</span></div><div class="title">'+
-    '<h1>#title#</h1><p class="task-category">#category#</p></div><p class="description">#desc#</p>'+
+    '<h1>#title#</h1><p class="#bgcolor#">#category#</p></div><p class="description">#desc#</p>'+
     '<p class="members">#members# Team Members</p><div class="members-img">#images#</div>';
 
     template = template.replace('#icon#', task.icon).replace('#title#', task.title).replace('#category#', task.category).replace('#desc#', task.desc);;
 
-    template = template.replace('#members#', task.members.length).replace('#bgcolor#',task.bgcolor)
+    template = template.replace('#members#', task.members.length).replaceAll('#bgcolor#',task.bgcolor)
 
     let imgs ="";
     task.members.forEach(x => {
@@ -26,8 +26,8 @@ function createTask(task) {
 let tasks = [
     {"icon":"auto_stories", "bgcolor":"orange", "title":"Do my home work", "category":"Education","desc":"Do all the home work by noon","members":['img/avatar.jpg','img/avatar.jpg','img/avatar.jpg']},
     {"icon":"medication", "bgcolor":"purple", "title":"Run 3 miles", "category":"Physical","desc":"Run 3 miles every day","members":['img/avatar.jpg','img/avatar.jpg']},
-    {"icon":"emoji_emotions", "bgcolor":"green", "title":"Run 3 miles", "category":"Physical","desc":"Run 3 miles every day","members":['img/avatar.jpg','img/avatar.jpg','img/avatar.jpg']},
-    {"icon":"question_answer", "bgcolor":"yellow", "title":"Run 3 miles", "category":"Physical","desc":"Run 3 miles every day","members":['img/avatar.jpg','img/avatar.jpg','img/avatar.jpg']},
+    {"icon":"emoji_emotions", "bgcolor":"green", "title":"Run 3 miles", "category":"Emotional","desc":"Run 3 miles every day","members":['img/avatar.jpg','img/avatar.jpg','img/avatar.jpg']},
+    {"icon":"question_answer", "bgcolor":"yellow", "title":"Run 3 miles", "category":"Social","desc":"Run 3 miles every day","members":['img/avatar.jpg','img/avatar.jpg','img/avatar.jpg']},
 ]
 
 tasks.forEach(createTask);
